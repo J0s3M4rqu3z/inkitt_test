887aa7c0155183789bec969ffac631b670045016
J0s3M4rqu3z <32398315+J0s3M4rqu3z@users.noreply.github.com>
2017-11-03 21:07:09 -0400

    Create sql_queries.sql
    This is the sql_queries asked on the first question

---
+++ sql_queries.sql
@@ -0,0 +1,34 @@
+#how much did they read? if we uderstand the quantyty as number of entrancies per day 
+SELECT visitor_id,count(reading.visitor_id) quantity_by_day,date(created_at) as reading_day
+FROM reading
+left join stories on reading.story_id = stories.id 
+where concat(category_one,' ',category_two) regexp 'horror'
+group by visitor_id,date(created_at);
+
+#how much did they read? if we uderstand the quantyty as seconds that the visitor was in ur plataform in total
+#but for the data kind i think is the first one
+SELECT visitor_id,sum(timestampdiff(second,tracking_time,created_at)) as quantity_by_day,date(created_at) as reading_day
+FROM reading
+left join stories on reading.story_id = stories.id 
+where concat(category_one,' ',category_two) regexp 'horror'
+group by visitor_id,date(created_at);
+
+#how many readers are there?
+
+SELECT count(distinct reading.visitor_id) quantity_by_day,date(created_at) as reading_day
+FROM reading
+left join stories on reading.story_id = stories.id 
+left join visits on visits.visitor_id = reading.visitor_id
+where concat(category_one,' ',category_two) regexp 'horror'
+group by date(created_at);
+
+#what country are the readers from?, i didn't put the date on the filter because
+#the country of the visitor will not change so its unrelevant
+#and i understant that the question was to read it 
+
+SELECT country, reading.visitor_id
+FROM reading
+left join stories on reading.story_id = stories.id 
+left join visits on visits.visitor_id = reading.visitor_id
+where concat(category_one,' ',category_two) regexp 'horror'
+group by (reading.visitor_id);
